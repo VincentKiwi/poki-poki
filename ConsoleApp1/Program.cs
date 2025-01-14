2eeb69617d949378e91ef242dc1d75837312d318
VincentKiwi <vincent.kiwanuka@elev.ga.ntig.se>
2025-01-14 11:08:24 +0100

    digimon klar

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,15 +8,9 @@ client.BaseAddress = new("https://digi-api.com/api/v1/digimon/");
 HttpResponseMessage response = client.GetAsync("1460").Result;
 
 digimon digimon = response.Content.ReadFromJsonAsync<digimon>().Result;
-// digimon = JsonSerializer.Deserialize<digimon>(1460);
 
 Console.WriteLine(digimon.name);
 
 Console.ReadLine();
 
 
-
-////pokemon p = JsonSerializer.Deserialize<pokemon>(snorlax);
-//Console.WriteLine(p.weight);
-
-//File.WriteAllText("snorlax.json", snorlax);
